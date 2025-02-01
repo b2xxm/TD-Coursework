9cc1639812e91acf19f203f94b9d9790f195a65f
b2xxm <privatehvht@gmail.com>
2025-02-01 22:35:17 +0000

    health bar
    also edited health setter and added backing field

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Base : MonoBehaviour
 {
-    [SerializeField] private int health;
+    private int health;
 
-    public int Health { get { return health; } }
+    [SerializeField] private int maxHealth;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TMP_Text display;
+
+    public int Health {
+        get => health;
+        private set {
+            if (health == value)
+                return;
+
+            health = value;
+
+            slider.value = (float) health / maxHealth;
+            display.SetText($"{health} / {maxHealth}");
+        }
+    }
+
+    public void Awake()
+    {
+        ResetBase();
+    }
 
     public void TakeDamage(int amount)
     {
-        health = Mathf.Max(health - amount, 0);
+        Health = Mathf.Max(Health - amount, 0);
 
-        if (health == 0) {
+        if (Health == 0) {
             // stop schedule
             // stop completely
         }
+    }
+
+    public void ResetBase()
+    {
+        Health = maxHealth;
     }
 }
